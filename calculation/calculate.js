1b7b1994ca808ca76b2403d2839258e6421ee586
Denny Setyawan Huda <dennshgit@gmail.com>
2022-09-28 11:52:10 +0700

    feat: add multiplication function

--- calculation/calculate.js
+++ calculation/calculate.js
@@ -5,3 +5,7 @@ export function summation(firstNumber, secondNumber) {
 export function subtraction(firstNumber, secondNumber) {
   return firstNumber - secondNumber;
 }
+
+export function multiplication(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
