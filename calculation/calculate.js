37e26bfa36aac2bcd1b86b00958d0b6b68b4c951
Denny Setyawan Huda <dennshgit@gmail.com>
2022-09-28 11:52:41 +0700

    feat: add division function

--- calculation/calculate.js
+++ calculation/calculate.js
@@ -9,3 +9,7 @@ export function subtraction(firstNumber, secondNumber) {
 export function multiplication(firstNumber, secondNumber) {
   return firstNumber * secondNumber;
 }
+
+export function division(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
