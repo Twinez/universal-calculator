6f95d249546f2dc09dc69f1403e415b255092b59
Denny Setyawan Huda <dennshgit@gmail.com>
2022-09-28 11:54:25 +0700

    feat: import module and console log function

---
+++ index.js
@@ -0,0 +1,14 @@
+import {
+  summation,
+  subtraction,
+  multiplication,
+  division,
+} from "./calculation/calculate.js";
+
+console.log(`Summation: ${summation(3, 5)}`);
+
+console.log(`Subtraction ${subtraction(6, 3)}`);
+
+console.log(`Mulitplication ${multiplication(3, 4)}`);
+
+console.log(`Division ${division(4, 2)}`);
