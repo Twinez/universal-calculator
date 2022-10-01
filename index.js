810db4fee43b6f76508c5aff2b3bdf515ec59c4c
Denny Setyawan Huda <dennshgit@gmail.com>
2022-10-02 00:35:56 +0700

    feat: import module and display calculation result

--- index.js
+++ index.js
@@ -5,10 +5,98 @@ import {
   division,
 } from "./calculation/calculate.js";
 
-console.log(`Summation: ${summation(3, 5)}`);
+import {
+  calculateSquareArea,
+  calculateSquarePerimeter,
+  calculateTriangleArea,
+  calculateTrianglePerimeter,
+  calculateCircleArea,
+  calculateCirclePerimeter,
+} from "./calculation/shape.js";
 
-console.log(`Subtraction ${subtraction(6, 3)}`);
+const squareFormElement = document.getElementById("square-form");
+const squareSideElement = document.getElementById("side-square");
+const squareButtonElement = document.getElementById("square");
+const squareWrapperElement = document.querySelector(".square-wrapper");
+const squareResultAreaElement = document.getElementById("square-area");
+const squareResultPerimeterElement =
+  document.getElementById("square-perimeter");
 
-console.log(`Mulitplication ${multiplication(3, 4)}`);
+const triangleWrapperElement = document.querySelector(".triangle-wrapper");
+const triangleFormElement = document.getElementById("triangle-form");
+const triangleBaseElement = document.getElementById("triangle-base");
+const triangleHeightElement = document.getElementById("triangle-height");
+const triangleButtonElement = document.getElementById("triangle");
+const triangleAreaElement = document.getElementById("triangle-area");
+const trianglePerimeterElement = document.getElementById("triangle-perimeter");
 
-console.log(`Division ${division(4, 2)}`);
+const circleWrapperElement = document.querySelector(".circle-wrapper");
+const circleFormElement = document.getElementById("circle-form");
+const circleRadiusElement = document.getElementById("circle-radius");
+const circleAreaElement = document.getElementById("circle-area");
+const circlePerimeterElement = document.getElementById("circle-perimeter");
+const circleButtonElement = document.getElementById("circle");
+
+window.addEventListener("load", () => {
+  triangleWrapperElement.style.display = "";
+  squareWrapperElement.style.display = "none";
+  circleWrapperElement.style.display = "none";
+});
+
+squareButtonElement.onclick = () => {
+  console.log("work");
+  squareWrapperElement.style.display = "";
+  triangleWrapperElement.style.display = "none";
+  circleWrapperElement.style.display = "none";
+};
+
+squareFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const resultArea = calculateSquareArea(squareSideElement.value);
+  squareResultAreaElement.innerText = ` Square Area: ${resultArea}`;
+
+  const resultPerimeter = calculateSquarePerimeter(squareSideElement.value);
+  squareResultPerimeterElement.innerText = `Square Perimeter: ${resultPerimeter}`;
+});
+
+triangleButtonElement.onclick = () => {
+  console.log("work");
+  triangleWrapperElement.style.display = "";
+  squareWrapperElement.style.display = "none";
+  circleWrapperElement.style.display = "none";
+};
+
+triangleFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const resultArea = calculateTriangleArea(
+    triangleBaseElement.value,
+    triangleHeightElement.value
+  ).toFixed(2);
+
+  triangleAreaElement.innerText = `Triangle Area: ${resultArea}`;
+  const resultPerimeter = calculateTrianglePerimeter(
+    triangleBaseElement.value,
+    triangleHeightElement.value
+  ).toFixed(2);
+
+  trianglePerimeterElement.innerText = `Triangle Perimeter ${resultPerimeter}`;
+});
+
+circleButtonElement.onclick = () => {
+  console.log("work");
+  circleWrapperElement.style.display = "";
+  squareWrapperElement.style.display = "none";
+  triangleWrapperElement.style.display = "none";
+};
+
+circleFormElement.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const resultArea = calculateCircleArea(circleRadiusElement.value).toFixed(2);
+
+  circleAreaElement.innerText = `Circle Area: ${resultArea}`;
+  const resultPerimeter = calculateCirclePerimeter(
+    circleRadiusElement.value
+  ).toFixed(2);
+
+  circlePerimeterElement.innerText = `Circle Perimeter ${resultPerimeter}`;
+});
